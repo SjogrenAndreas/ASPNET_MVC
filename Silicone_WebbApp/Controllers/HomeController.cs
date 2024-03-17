aaa0e6897b21bcb8babfecd76f5d690483611a6e
Andreas <143494824+Adoroar@users.noreply.github.com>
2024-03-17 18:08:45 +0100

    Add project files.

---
+++ Silicone_WebbApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicone_WebbApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
